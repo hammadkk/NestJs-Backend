bb88d8db766532845b121877169e3efeb432c99b
hammadkk <hammadsaif061@gmail.com>
2024-08-14 17:02:01 +0500

    added webpack conf

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { GlobalExceptionFilter } from './common/filters/global-exception.filter'
 import { ConfigService } from '@nestjs/config';
 // import { SeedService } from './seed/seed.service';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
@@ -13,5 +15,10 @@ async function bootstrap() {
   // await seedService.seed();
   const configService = app.get(ConfigService);
   await app.listen(configService.get<number>('port'));
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
